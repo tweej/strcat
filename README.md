a607013ea232b85b15e90ef2f9254e98fc8bf39a
T.J. Mercier <thomas.mercier.jr@gmail.com>
2017-11-25 16:07:32 -0800

    Add unoptimized benchmarks for comparison

--- README.md
+++ README.md
@@ -3,10 +3,12 @@ Minimalistic, header-only strcat implementation that's still fast and efficient
 
 The codebase I work on most is limited to C++11 and as much as I'd like to add Abseil as a dependency for newer C++ features, that's not something that's palatable at the moment.
 
-One of the things I see others almost always do inefficiently is string concatenation. Almost always that's for logging, so it's something that appears frequently. This strcat implementation is intended to improve the performance of these string concatenations while avoiding an external dependency.
+One of the things I see others almost always do inefficiently is string concatenation. Almost always that's for logging, so it's something that appears frequently. This strcat implementation is intended to improve the performance of these string concatenations while avoiding an external dependency and the verbosity of a more efficient manual implementation at each call site.
 
 The performance is comparable to Abseil's StrCat in the included benchmarks:
 
+![](https://imgur.com/11T5zu9.png)
+![](https://imgur.com/lLL5DJy.png)
 
 GCC 7.1;
 Ubuntu 16.04;
@@ -21,45 +23,57 @@ CPU Caches:
   L1 Instruction 32K
   L2 Unified 256K
   L3 Unified 4096K
--------------------------------------------------------------------------
-Benchmark                                  Time           CPU Iterations
--------------------------------------------------------------------------
-BM_Baseline_ShortStringLiteral_2          18 ns         18 ns   38344665
-BM_Minimal_ShortStringLiteral_2           19 ns         19 ns   36855559
-BM_Abseil_ShortStringLiteral_2            16 ns         16 ns   44297477
-BM_Baseline_ShortStringLiteral_10         89 ns         89 ns    7804995
-BM_Minimal_ShortStringLiteral_10          88 ns         88 ns    7832701
-BM_Abseil_ShortStringLiteral_10           80 ns         80 ns    8664640
-BM_Baseline_LongStringLiteral_2           40 ns         40 ns   17473436
-BM_Minimal_LongStringLiteral_2            40 ns         40 ns   17540705
-BM_Abseil_LongStringLiteral_2             46 ns         46 ns   15492763
-BM_Baseline_LongStringLiteral_10         118 ns        118 ns    5811316
-BM_Minimal_LongStringLiteral_10          120 ns        120 ns    5812499
-BM_Abseil_LongStringLiteral_10           119 ns        119 ns    5900929
-BM_Baseline_ShortStrPtr_2                 18 ns         18 ns   39267590
-BM_Minimal_ShortStrPtr_2                  18 ns         18 ns   38940780
-BM_Abseil_ShortStrPtr_2                   15 ns         15 ns   45716809
-BM_Baseline_ShortStrPtr_10                89 ns         89 ns    7897463
-BM_Minimal_ShortStrPtr_10                 88 ns         88 ns    7927259
-BM_Abseil_ShortStrPtr_10                  80 ns         80 ns    8850553
-BM_Baseline_LongStrPtr_2                  40 ns         40 ns   17346132
-BM_Minimal_LongStrPtr_2                   40 ns         40 ns   17728763
-BM_Abseil_LongStrPtr_2                    45 ns         45 ns   15457360
-BM_Baseline_LongStrPtr_10                118 ns        118 ns    5926412
-BM_Minimal_LongStrPtr_10                 119 ns        119 ns    5870746
-BM_Abseil_LongStrPtr_10                  119 ns        119 ns    5897610
-BM_Baseline_ShortSTLStr_2                 18 ns         18 ns   39326866
-BM_Minimal_ShortSTLStr_2                  18 ns         18 ns   38624694
-BM_Abseil_ShortSTLStr_2                   15 ns         15 ns   45917852
-BM_Baseline_ShortSTLStr_10                88 ns         88 ns    7926536
-BM_Minimal_ShortSTLStr_10                 90 ns         90 ns    7743429
-BM_Abseil_ShortSTLStr_10                  80 ns         80 ns    8832356
-BM_Baseline_LongSTLStr_2                  55 ns         55 ns   12513462
-BM_Minimal_LongSTLStr_2                   39 ns         39 ns   17713246
-BM_Abseil_LongSTLStr_2                    45 ns         45 ns   15454816
-BM_Baseline_LongSTLStr_10                178 ns        178 ns    3924503
-BM_Minimal_LongSTLStr_10                 127 ns        127 ns    5494250
-BM_Abseil_LongSTLStr_10                  118 ns        118 ns    5901232
+----------------------------------------------------------------------------
+Benchmark                                     Time           CPU Iterations
+----------------------------------------------------------------------------
+BM_Unoptimized_ShortStringLiteral_2          13 ns         13 ns   52013487
+BM_Baseline_ShortStringLiteral_2             18 ns         18 ns   38344665
+BM_Minimal_ShortStringLiteral_2              19 ns         19 ns   36855559
+BM_Abseil_ShortStringLiteral_2               16 ns         16 ns   44297477
+BM_Unoptimized_ShortStringLiteral_10        113 ns        113 ns    6195967
+BM_Baseline_ShortStringLiteral_10            89 ns         89 ns    7804995
+BM_Minimal_ShortStringLiteral_10             88 ns         88 ns    7832701
+BM_Abseil_ShortStringLiteral_10              80 ns         80 ns    8664640
+BM_Unoptimized_LongStringLiteral_2           64 ns         64 ns   10901975
+BM_Baseline_LongStringLiteral_2              40 ns         40 ns   17473436
+BM_Minimal_LongStringLiteral_2               40 ns         40 ns   17540705
+BM_Abseil_LongStringLiteral_2                46 ns         46 ns   15492763
+BM_Unoptimized_LongStringLiteral_10         332 ns        332 ns    2107163
+BM_Baseline_LongStringLiteral_10            118 ns        118 ns    5811316
+BM_Minimal_LongStringLiteral_10             120 ns        120 ns    5812499
+BM_Abseil_LongStringLiteral_10              119 ns        119 ns    5900929
+BM_Unoptimized_ShortStrPtr_2                 12 ns         12 ns   59491594
+BM_Baseline_ShortStrPtr_2                    18 ns         18 ns   39267590
+BM_Minimal_ShortStrPtr_2                     18 ns         18 ns   38940780
+BM_Abseil_ShortStrPtr_2                      15 ns         15 ns   45716809
+BM_Unoptimized_ShortStrPtr_10               112 ns        112 ns    6233577
+BM_Baseline_ShortStrPtr_10                   89 ns         89 ns    7897463
+BM_Minimal_ShortStrPtr_10                    88 ns         88 ns    7927259
+BM_Abseil_ShortStrPtr_10                     80 ns         80 ns    8850553
+BM_Unoptimized_LongStrPtr_2                  62 ns         62 ns   11056430
+BM_Baseline_LongStrPtr_2                     40 ns         40 ns   17346132
+BM_Minimal_LongStrPtr_2                      40 ns         40 ns   17728763
+BM_Abseil_LongStrPtr_2                       45 ns         45 ns   15457360
+BM_Unoptimized_LongStrPtr_10                332 ns        332 ns    2107739
+BM_Baseline_LongStrPtr_10                   118 ns        118 ns    5926412
+BM_Minimal_LongStrPtr_10                    119 ns        119 ns    5870746
+BM_Abseil_LongStrPtr_10                     119 ns        119 ns    5897610
+BM_Unoptimized_ShortSTLStr_2                 15 ns         15 ns   46635091
+BM_Baseline_ShortSTLStr_2                    18 ns         18 ns   39326866
+BM_Minimal_ShortSTLStr_2                     18 ns         18 ns   38624694
+BM_Abseil_ShortSTLStr_2                      15 ns         15 ns   45917852
+BM_Unoptimized_ShortSTLStr_10               112 ns        112 ns    6227649
+BM_Baseline_ShortSTLStr_10                   88 ns         88 ns    7926536
+BM_Minimal_ShortSTLStr_10                    90 ns         90 ns    7743429
+BM_Abseil_ShortSTLStr_10                     80 ns         80 ns    8832356
+BM_Unoptimized_LongSTLStr_2                  66 ns         65 ns   10648803
+BM_Baseline_LongSTLStr_2                     55 ns         55 ns   12513462
+BM_Minimal_LongSTLStr_2                      39 ns         39 ns   17713246
+BM_Abseil_LongSTLStr_2                       45 ns         45 ns   15454816
+BM_Unoptimized_LongSTLStr_10                318 ns        318 ns    2228679
+BM_Baseline_LongSTLStr_10                   178 ns        178 ns    3924503
+BM_Minimal_LongSTLStr_10                    127 ns        127 ns    5494250
+BM_Abseil_LongSTLStr_10                     118 ns        118 ns    5901232
 
 *** Finished ***
 ```
--- microbench.cpp
+++ microbench.cpp
@@ -3,10 +3,16 @@
 #include "absl/strings/str_cat.h"
 #include "strcat.h"
 
+static void BM_Unoptimized_ShortStringLiteral_2(benchmark::State& state) {
+    for(auto _ : state) {
+        const std::string s = std::string("foo") + "bar";
+    }
+} BENCHMARK(BM_Unoptimized_ShortStringLiteral_2);
+
 static void BM_Baseline_ShortStringLiteral_2(benchmark::State& state) {
     for(auto _ : state) {
         std::string s;
-        s.reserve(6); 
+        s.reserve(6);
         s.append("foo").append("bar");
     }
 } BENCHMARK(BM_Baseline_ShortStringLiteral_2);
@@ -24,6 +30,13 @@ static void BM_Abseil_ShortStringLiteral_2(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_ShortStringLiteral_10(benchmark::State& state) {
+    for(auto _ : state) {
+        std::string s = std::string("foo") + "bar" + "bat" + "hat" + "cat"
+            + "dog" + "bog" + "cog" + "log" + "sob";
+    }
+} BENCHMARK(BM_Unoptimized_ShortStringLiteral_10);
+
 static void BM_Baseline_ShortStringLiteral_10(benchmark::State& state) {
     for(auto _ : state) {
         std::string s;
@@ -32,7 +45,7 @@ static void BM_Baseline_ShortStringLiteral_10(benchmark::State& state) {
             .append("cat").append("dog").append("bog").append("cog")
             .append("log").append("sob");
     }
-      
+
 } BENCHMARK(BM_Baseline_ShortStringLiteral_10);
 
 static void BM_Minimal_ShortStringLiteral_10(benchmark::State& state) {
@@ -50,10 +63,17 @@ static void BM_Abseil_ShortStringLiteral_10(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_LongStringLiteral_2(benchmark::State& state) {
+    for(auto _ : state) {
+        std::string s = std::string("012345678901234567890123456789") +
+            "123456789012345678901234567890";
+    }
+} BENCHMARK(BM_Unoptimized_LongStringLiteral_2);
+
 static void BM_Baseline_LongStringLiteral_2(benchmark::State& state) {
     for(auto _ : state) {
         std::string s;
-        s.reserve(60); 
+        s.reserve(60);
         s.append("012345678901234567890123456789")
             .append("123456789012345678901234567890");
     }
@@ -74,10 +94,21 @@ static void BM_Abseil_LongStringLiteral_2(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_LongStringLiteral_10(benchmark::State& state) {
+    for(auto _ : state) {
+        std::string s = std::string("012345678901234567890123456789") +
+            "123456789012345678901234567890" + "234567890123456789012345678901" +
+            "345678901234567890123456789012" + "456789012345678901234567890123" +
+            "567890123456789012345678901234" + "678901234567890123456789012345" +
+            "789012345678901234567890123456" + "890123456789012345678901234567" +
+            "901234567890123456789012345678";
+    }
+} BENCHMARK(BM_Unoptimized_LongStringLiteral_10);
+
 static void BM_Baseline_LongStringLiteral_10(benchmark::State& state) {
     for(auto _ : state) {
         std::string s;
-        s.reserve(300); 
+        s.reserve(300);
         s.append("012345678901234567890123456789")
             .append("123456789012345678901234567890").append("234567890123456789012345678901")
             .append("345678901234567890123456789012").append("456789012345678901234567890123")
@@ -110,6 +141,15 @@ static void BM_Abseil_LongStringLiteral_10(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_ShortStrPtr_2(benchmark::State& state) {
+    // Pretend we don't know the length of the pointed-to strings
+    const char * const s0 = "foo";
+    const char * const s1 = "bar";
+    for(auto _ : state) {
+        std::string s = std::string(s0) + s1;
+    }
+} BENCHMARK(BM_Unoptimized_ShortStrPtr_2);
+
 static void BM_Baseline_ShortStrPtr_2(benchmark::State& state) {
     // Pretend we don't know the length of the pointed-to strings
     const char * const s0 = "foo";
@@ -138,6 +178,24 @@ static void BM_Abseil_ShortStrPtr_2(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_ShortStrPtr_10(benchmark::State& state) {
+    // Pretend we don't know the length of the pointed-to strings
+    const char * const s0 = "foo";
+    const char * const s1 = "bar";
+    const char * const s2 = "bat";
+    const char * const s3 = "hat";
+    const char * const s4 = "cat";
+    const char * const s5 = "dog";
+    const char * const s6 = "bog";
+    const char * const s7 = "cog";
+    const char * const s8 = "log";
+    const char * const s9 = "sob";
+    for(auto _ : state) {
+        std::string s = std::string(s0) + s1 + s2 + s3 + s4 +
+            s5 + s6 + s7 + s8 + s9;
+    }
+} BENCHMARK(BM_Unoptimized_ShortStrPtr_10);
+
 static void BM_Baseline_ShortStrPtr_10(benchmark::State& state) {
     // Pretend we don't know the length of the pointed-to strings
     const char * const s0 = "foo";
@@ -153,10 +211,10 @@ static void BM_Baseline_ShortStrPtr_10(benchmark::State& state) {
     for(auto _ : state) {
         std::string s;
         using std::strlen;
-        
+
         s.reserve(strlen(s0) + strlen(s1) + strlen(s2) + strlen(s3) + strlen(s4) +
             strlen(s5) + strlen(s6) + strlen(s7) + strlen(s8) + strlen(s9));
-        
+
         s.append(s0).append(s1).append(s2).append(s3).append(s4).append(s5)
             .append(s6).append(s7).append(s8).append(s9);
     }
@@ -196,6 +254,15 @@ static void BM_Abseil_ShortStrPtr_10(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_LongStrPtr_2(benchmark::State& state) {
+    // Pretend we don't know the length of the pointed-to strings
+    const char * const s0 = "012345678901234567890123456789";
+    const char * const s1 = "123456789012345678901234567890";
+    for(auto _ : state) {
+        std::string s = std::string(s0) + s1;
+    }
+} BENCHMARK(BM_Unoptimized_LongStrPtr_2);
+
 static void BM_Baseline_LongStrPtr_2(benchmark::State& state) {
     // Pretend we don't know the length of the pointed-to strings
     const char * const s0 = "012345678901234567890123456789";
@@ -225,6 +292,24 @@ static void BM_Abseil_LongStrPtr_2(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_LongStrPtr_10(benchmark::State& state) {
+    // Pretend we don't know the length of the pointed-to strings
+    const char * const s0 = "012345678901234567890123456789";
+    const char * const s1 = "123456789012345678901234567890";
+    const char * const s2 = "234567890123456789012345678901";
+    const char * const s3 = "345678901234567890123456789012";
+    const char * const s4 = "456789012345678901234567890123";
+    const char * const s5 = "567890123456789012345678901234";
+    const char * const s6 = "678901234567890123456789012345";
+    const char * const s7 = "789012345678901234567890123456";
+    const char * const s8 = "890123456789012345678901234567";
+    const char * const s9 = "901234567890123456789012345678";
+    for(auto _ : state) {
+        std::string s = std::string(s0) + s1 + s2 + s3 + s4 +
+            s5 + s6 + s7 + s8 + s9;
+    }
+} BENCHMARK(BM_Unoptimized_LongStrPtr_10);
+
 static void BM_Baseline_LongStrPtr_10(benchmark::State& state) {
     // Pretend we don't know the length of the pointed-to strings
     const char * const s0 = "012345678901234567890123456789";
@@ -240,10 +325,10 @@ static void BM_Baseline_LongStrPtr_10(benchmark::State& state) {
     for(auto _ : state) {
         std::string s;
         using std::strlen;
-        
+
         s.reserve(strlen(s0) + strlen(s1) + strlen(s2) + strlen(s3) + strlen(s4) +
             strlen(s5) + strlen(s6) + strlen(s7) + strlen(s8) + strlen(s9));
-        
+
         s.append(s0).append(s1).append(s2).append(s3).append(s4).append(s5)
             .append(s6).append(s7).append(s8).append(s9);
     }
@@ -283,6 +368,15 @@ static void BM_Abseil_LongStrPtr_10(benchmark::State& state) {
 
 
 
+
+static void BM_Unoptimized_ShortSTLStr_2(benchmark::State& state) {
+    const std::string s0{"foo"};
+    const std::string s1{"bar"};
+    for(auto _ : state) {
+        std::string s = std::string(s0) + s1;
+    }
+} BENCHMARK(BM_Unoptimized_ShortSTLStr_2);
+
 static void BM_Baseline_ShortSTLStr_2(benchmark::State& state) {
     const std::string s0{"foo"};
     const std::string s1{"bar"};
@@ -310,6 +404,22 @@ static void BM_Abseil_ShortSTLStr_2(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_ShortSTLStr_10(benchmark::State& state) {
+    const std::string s0{"foo"};
+    const std::string s1{"bar"};
+    const std::string s2{"bat"};
+    const std::string s3{"hat"};
+    const std::string s4{"cat"};
+    const std::string s5{"dog"};
+    const std::string s6{"bog"};
+    const std::string s7{"cog"};
+    const std::string s8{"log"};
+    const std::string s9{"sob"};
+    for(auto _ : state) {
+        std::string s = s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9;
+    }
+} BENCHMARK(BM_Unoptimized_ShortSTLStr_10);
+
 static void BM_Baseline_ShortSTLStr_10(benchmark::State& state) {
     const std::string s0{"foo"};
     const std::string s1{"bar"};
@@ -326,7 +436,7 @@ static void BM_Baseline_ShortSTLStr_10(benchmark::State& state) {
         s.reserve(s0.length() + s1.length() + s2.length() + s3.length()
             + s4.length() + s5.length() + s6.length() + s8.length()
             + s9.length());
-        
+
         s.append(s0).append(s1).append(s2).append(s3).append(s4).append(s5)
             .append(s6).append(s7).append(s8).append(s9);
     }
@@ -365,6 +475,14 @@ static void BM_Abseil_ShortSTLStr_10(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_LongSTLStr_2(benchmark::State& state) {
+    const std::string s0{"012345678901234567890123456789"};
+    const std::string s1{"123456789012345678901234567890"};
+    for(auto _ : state) {
+        std::string s = s0 + s1;
+    }
+} BENCHMARK(BM_Unoptimized_LongSTLStr_2);
+
 static void BM_Baseline_LongSTLStr_2(benchmark::State& state) {
     const std::string s0{"012345678901234567890123456789"};
     const std::string s1{"123456789012345678901234567890"};
@@ -392,6 +510,22 @@ static void BM_Abseil_LongSTLStr_2(benchmark::State& state) {
 
 
 
+static void BM_Unoptimized_LongSTLStr_10(benchmark::State& state) {
+    const std::string s0{"012345678901234567890123456789"};
+    const std::string s1{"123456789012345678901234567890"};
+    const std::string s2{"234567890123456789012345678901"};
+    const std::string s3{"345678901234567890123456789012"};
+    const std::string s4{"456789012345678901234567890123"};
+    const std::string s5{"567890123456789012345678901234"};
+    const std::string s6{"678901234567890123456789012345"};
+    const std::string s7{"789012345678901234567890123456"};
+    const std::string s8{"890123456789012345678901234567"};
+    const std::string s9{"901234567890123456789012345678"};
+    for(auto _ : state) {
+        std::string s = s0 + s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8 + s9;
+    }
+} BENCHMARK(BM_Unoptimized_LongSTLStr_10);
+
 static void BM_Baseline_LongSTLStr_10(benchmark::State& state) {
     const std::string s0{"012345678901234567890123456789"};
     const std::string s1{"123456789012345678901234567890"};
@@ -408,7 +542,7 @@ static void BM_Baseline_LongSTLStr_10(benchmark::State& state) {
         s.reserve(s0.length() + s1.length() + s2.length() + s3.length()
             + s4.length() + s5.length() + s6.length() + s8.length()
             + s9.length());
-        
+
         s.append(s0).append(s1).append(s2).append(s3).append(s4).append(s5)
             .append(s6).append(s7).append(s8).append(s9);
     }
