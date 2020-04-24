e8108adccd293d66cb1e77e1c1d0aa1bdb06432c
T.J. Mercier <mercthom@lab126.com>
2020-04-24 13:51:04 -0700

    Replace parameter pack expansion with fold expressions

--- strcat.h
+++ strcat.h
@@ -1,7 +1,6 @@
 #ifndef STRCAT_H
 #define STRCAT_H
 
-#include <cstddef>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -9,42 +8,20 @@
 namespace minimal
 {
 
-namespace { // internal linkage
-    constexpr std::size_t adder() noexcept { return 0; }
-
-    constexpr std::size_t adder(const std::size_t size) noexcept
-    { return size; }
-
-    template <typename ... Sizes>
-    constexpr std::size_t adder(const std::size_t first, const Sizes ... args) noexcept
-    { return first + adder(args...); }
-
-    constexpr void append(const std::string &s) noexcept {}
-
-    template <typename T, typename ... Args>
-    constexpr void append(std::string &s, T && first, Args && ... others) noexcept
-    {
-        // should not throw; already allocated and reserve() would have
-        // also thrown for size() > max_size()
-        s.append(static_cast<std::string_view>(first));
-        append(s, std::forward<Args>(others)...);
-    }
-} // unnamed namespace
-
 template <typename ... Str>
 constexpr std::string strcat(
     std::string_view first,
     std::string_view second,
     Str && ...       others) 
 {
-    const std::size_t sum = first.size() + second.size() +
-        adder((static_cast<std::string_view>(others).size())...);
+    const std::string::size_type sum = first.size() + second.size() +
+        (static_cast<std::string_view>(others).size() + ... + 0);
 
     std::string ret;
     ret.reserve(sum); // allocate once
 
     ret.append(first).append(second);
-    append(ret, std::forward<Str>(others)...);
+    (ret.append(std::forward<Str>(others)), ...);
 
     return ret;
 }
